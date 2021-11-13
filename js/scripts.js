1ca8565b900fcd1d0bc8285229ea39ae997ffb86
Anil Duyguc <anldygc35@windowslive.com>
2021-11-13 16:38:52 +0300

    grunt part-1

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,22 @@
- //  $(document).ready(function(){
-        //      $('[data-bs-toggle="tooltip"]').Tooltip();
-        //  });
-
-
-        //  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-        //  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-        //      return new bootstrap.Tooltip(tooltipTriggerEl)
-        //  });
-
-        $(document).ready(function () {
-            $('#myCarousel').carousel({
-                interval: 2000
-            });
-            $('#carouselButton').on('click', function () {
-                if ($('#carouselButton').children('span').hasClass('fa-pause')) {
-                    $('#myCarousel').carousel("pause");
-                    $('#carouselButton').children('span').removeClass('fa-pause');
-                    $('#carouselButton').children('span').addClass('fa-play');
-                }
-                else if ($('#carouselButton').children('span').hasClass('fa-play')) {
-                    $('#myCarousel').carousel("cycle");
-                    $('#carouselButton').children('span').removeClass('fa-play');
-                    $('#carouselButton').children('span').addClass('fa-pause');
-                }
-            });
-            $('#loginButton').on('click',function(){
-                $('#loginModal').modal('show');
-            });
-            $('#tableButton').on('click',function(){
-                $('#reserveTable').modal('show');
-            });
-        });
+$(document).ready(function () {
+  $("#myCarousel").carousel({
+    interval: 2000,
+  });
+  $("#carouselButton").on("click", function () {
+    if ($("#carouselButton").children("span").hasClass("fa-pause")) {
+      $("#myCarousel").carousel("pause");
+      $("#carouselButton").children("span").removeClass("fa-pause");
+      $("#carouselButton").children("span").addClass("fa-play");
+    } else if ($("#carouselButton").children("span").hasClass("fa-play")) {
+      $("#myCarousel").carousel("cycle");
+      $("#carouselButton").children("span").removeClass("fa-play");
+      $("#carouselButton").children("span").addClass("fa-pause");
+    }
+  });
+  $("#loginButton").on("click", function () {
+    $("#loginModal").modal("show");
+  });
+  $("#tableButton").on("click", function () {
+    $("#reserveTable").modal("show");
+  });
+});
